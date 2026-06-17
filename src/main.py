997a7ff3d9b70c7951beb8aa030aeb740b53982a
geebaby <ndai6653@gmail.com>
2026-06-17 09:17:09 +0700

    update v7 for prometheus fastapi

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
 import uuid
-from prometheus_fastapi_instrumentator import Instrumentator
-
 from src.core.config import settings
 from src.core.logger import logger
 from src.api.routes import extract, health
@@ -30,9 +28,6 @@ async def add_request_id_middleware(request: Request, call_next):
     response = await call_next(request)
     response.headers["X-Request-ID"] = request_id
     return response
-
-# Initialize Prometheus instrumentation
-Instrumentator().instrument(app).expose(app)
 
 app.include_router(health.router, prefix="/health", tags=["Health"])
 app.include_router(extract.router, prefix="/extract", tags=["Extraction"])
